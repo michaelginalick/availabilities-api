43ed434be47f0a1052736056d259deb20a7a33d9
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2019-04-06 17:50:45 -0500

    added limit method on scope model and spec

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,4 +2,9 @@ class Slot < ApplicationRecord
 
   validates :start_time, :end_time, presence: true
 
+  def self.scope(count)
+    return Slot.all unless count.present?
+    Slot.limit(count)
+  end
+  
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Slot, type: :model do
     it { should validate_presence_of(:end_time) }
   end
 
+  let!(:slots) { create_list(:slot, 10) }
 
+  describe "#scope" do
+    it "will limit the number of records returned" do
+      expect(Slot.scope(5).length).to eq(5)
+    end
+
+    it "will limit the number of records returned" do
+      expect(Slot.scope(nil).length).to eq(10)
+    end
+  end
 end
